42720c1430261b3a2744e0c0a515f7e0ccebfa27
edita21 <ekuberk0@chicagobooth.edu>
2021-07-11 22:03:18 +0000

    22%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,17 @@ end
 
 def calculate_square
 # params = {"elephant"=>"42"}
-@num = params.fetch("elephant").to_i
+@num = params.fetch("elephant").to_f
 @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb"})
 end    
+
+def calculate_random
+
+@lower = params.fetch("user_min").to_f 
+@upper = params.fetch("user_max").to_f 
+@result = rand(@lower..@upper)
+render({ :template => "calculation_templates/rand_results.html.erb"})
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ get("/square/new", { :controller => "application", :action => "blank_square_form
 
 get("/square/results", { :controller => "application", :action => "calculate_square"})
 
+get("/random/results", { :controller => "application", :action => "calculate_random"})
 end
