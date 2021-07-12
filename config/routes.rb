8e630411df2e00af2920a46dec6a1b22bfdec546
edita21 <ekuberk0@chicagobooth.edu>
2021-07-12 15:29:43 +0000

    commit with results

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ get("/square/new", { :controller => "application", :action => "blank_square_form
 
 get("/square/results", { :controller => "application", :action => "calculate_square"})
 
+
 get("/random/results", { :controller => "application", :action => "calculate_random"})
 
-
-
 get("/random/new", { :controller => "application", :action => "random_form"})
-
 
 
 get("/payment/new", { :controller => "application", :action => "payment_form"})
